190303762445db83920096c876119e341b39b7cf
Tobias McVey <tobias.mcvey@gmail.com>
2025-10-04 15:28:55 +0200

    bruk ny dataframe for regresjonsmodeller

--- src/brev_analyse/dagpenger.py
+++ src/brev_analyse/dagpenger.py
@@ -50,16 +50,16 @@ reg_df = df.copy()
 # %%
 # Logistisk regresjon
 # Årsaker til at innbyggere tar kontakt om brev
-subset["dep"] = subset["Kontaktet_Nav"]
-subset["dep"] = subset["dep"].map(
+reg_df["dep"] = reg_df["Kontaktet_Nav"]
+reg_df["dep"] = reg_df["dep"].map(
     {"Jeg tok ikke kontakt med NAV om brevet": 0, "Jeg kontaktet NAV om brevet": 1}
 )
-subset = subset.dropna(subset=["dep"])
-subset["dep"] = subset["dep"].astype(int)
+reg_df = reg_df.dropna(subset=["dep"])
+reg_df["dep"] = reg_df["dep"].astype(int)
 # %%
 # Logistisk regresjon
 # Kontaktet de Nav og morsmål
-model = logit("dep ~ C(Morsmål)", data=subset)
+model = logit("dep ~ C(Morsmål)", data=reg_df)
 res = model.fit()
 print("Modell nummer 1")
 print(f"Formel: {res.model.formula} \n \n")
@@ -67,7 +67,7 @@ print(res.summary())
 # %%
 # Log regresjon
 # Kontaktet de Nav og morsmål
-model = logit("dep ~ C(Alder)", data=subset)
+model = logit("dep ~ C(Alder)", data=reg_df)
 res = model.fit()
 print("Modell nummer 2")
 print(f"Formel: {res.model.formula} \n \n")
@@ -75,7 +75,7 @@ print(res.summary())
 # %%
 # Log regresjon
 # Kontaktet Nav og brevtype + alder + morsmål
-model = logit("dep ~ C(Brevtype) + C(Alder) + C(Morsmål)", data=subset)
+model = logit("dep ~ C(Brevtype) + C(Alder) + C(Morsmål)", data=reg_df)
 res = model.fit()
 print("Modell nummer 3")
 print(f"Formel: {res.model.formula} \n \n")
@@ -83,8 +83,8 @@ print(res.summary())
 # %%
 # Likelihood test for å se om alder forklarer mer enn nullhypotese - ingen variabel
 # Lag begge modellene og sammenlign fit
-model_with = logit("dep ~ C(Alder)", data=subset).fit()
-model_without = logit("dep ~ 1", data=subset).fit()
+model_with = logit("dep ~ C(Alder)", data=reg_df).fit()
+model_without = logit("dep ~ 1", data=reg_df).fit()
 
 # Extract log-likelihoods
 llf_with = model_with.llf
@@ -107,7 +107,7 @@ print(f"P-value: {p_value:.5f}")
 # %%
 # Log regresjon
 # Kontaktet Nav og alder og morsmål
-model = logit("dep ~ C(Morsmål) + C(Alder)", data=subset)
+model = logit("dep ~ C(Morsmål) + C(Alder)", data=reg_df)
 res = model.fit()
 print("Modell nummer 4")
 print(f"Formel: {res.model.formula} \n \n")
@@ -116,8 +116,8 @@ print(res.summary())
 # %%
 # Regresjoner om
 # De som synes det er lett eller veldig lett å forstå vedtak
-subset["dep"] = subset["Innvilgelse_1"].copy()
-subset["dep"] = subset["dep"].map(
+reg_df["dep"] = reg_df["Innvilgelse_1"].copy()
+reg_df["dep"] = reg_df["dep"].map(
     {
         "Lett å forstå": 1,
         "Veldig lett å forstå": 1,
@@ -127,11 +127,11 @@ subset["dep"] = subset["dep"].map(
         "Jeg fant ikke forklaringen": 0,
     }
 )
-subset = subset.dropna(subset=["dep"])
-subset["dep"] = subset["dep"].astype(int)
+reg_df = reg_df.dropna(subset=["dep"])
+reg_df["dep"] = reg_df["dep"].astype(int)
 # %%
 # Log regresjon - forstår begrunnelse og morsmål alder og kontaktet Nav
-model = logit("dep ~ C(Morsmål) + C(Alder) + C(Kontaktet_Nav)", data=subset)
+model = logit("dep ~ C(Morsmål) + C(Alder) + C(Kontaktet_Nav)", data=reg_df)
 res = model.fit()
 print("Modell nummer 5")
 print(f"Formelen: {res.model.formula} \n \n")
@@ -141,19 +141,19 @@ print(res.summary())
 # Før vi bytter avhengig variabel så må vi laste inn datasett på nytt
 # For å bli kvitt manglende svar
 
-subset = pd.read_spss(datasett_sav_sti)
+reg_df = pd.read_spss(datasett_sav_sti)
 # %%
 # Kombinere variablene om å forstå vedtak
 # kombinerer spørsmål om begrunnelse for alle brevtyper
-subset["dep_forstå"] = (
-    subset[["Innvilgelse_1", "Avslag_1", "Mangelbrev_1", "Stans_1"]]
+reg_df["dep_forstå"] = (
+    reg_df[["Innvilgelse_1", "Avslag_1", "Mangelbrev_1", "Stans_1"]]
     .bfill(axis=1)
     .iloc[:, 0]
 )
 
 # %%
 # sett nivåer på ordinal variabel
-subset["dep_forstå"] = subset["dep_forstå"].cat.set_categories(
+reg_df["dep_forstå"] = reg_df["dep_forstå"].cat.set_categories(
     [
         "Veldig vanskelig å forstå",
         "Vanskelig å forstå",
@@ -163,13 +163,13 @@ subset["dep_forstå"] = subset["dep_forstå"].cat.set_categories(
     ],
     ordered=True,
 )
-subset = subset.dropna(subset=["dep_forstå"])  # fjern tomme rader
+reg_df = reg_df.dropna(subset=["dep_forstå"])  # fjern tomme rader
 # %%
 # Ref https://github.com/statsmodels/statsmodels/issues/7418
 # I en regresjon med flere ordinale variabler kombinerer vi disse med egen syntaks
-# OrderedModel.from_formula('dependent ~ C(factor1) + C(factor2) + C(factor1):C(factor2)', distr='logit', data=subset)
+# OrderedModel.from_formula('dependent ~ C(factor1) + C(factor2) + C(factor1):C(factor2)', distr='logit', data=reg_df)
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(Alder) + C(Spraak) + C(Alder):C(Spraak)", distr="logit", data=subset
+    "dep_forstå ~ C(Alder) + C(Spraak) + C(Alder):C(Spraak)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 6")
@@ -177,21 +177,21 @@ print(f"Formelen: {res.model.formula} \n \n")
 print(res.summary())
 # %%
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(Alder) + C(Morsmål) + C(Alder):C(Morsmål)", distr="logit", data=subset
+    "dep_forstå ~ C(Alder) + C(Morsmål) + C(Alder):C(Morsmål)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 7")
 print(f"Formelen: {res.model.formula} \n \n")
 print(res.summary())
 # %%
-model = OrderedModel.from_formula("dep_forstå ~ C(Tid_brukt)", distr="logit", data=subset)
+model = OrderedModel.from_formula("dep_forstå ~ C(Tid_brukt)", distr="logit", data=reg_df)
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 8")
 print(f"Formelen: {res.model.formula} \n \n")
 print(res.summary())
 # %%
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(Klagerettigheter)", distr="logit", data=subset
+    "dep_forstå ~ C(Klagerettigheter)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 9")
@@ -199,7 +199,7 @@ print(f"Formelen: {res.model.formula} \n \n")
 print(res.summary())
 # %%
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(Antall_ganger_lest)", distr="logit", data=subset
+    "dep_forstå ~ C(Antall_ganger_lest)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 10")
@@ -207,10 +207,10 @@ print(f"Formelen: {res.model.formula} \n \n")
 print(res.summary())
 # %%
 # forståelse av begrunnelse i vedtak og brevtype
-subset["brev"] = subset["Brevtype"].copy()
-subset["brev"] = subset["brev"].str.replace("Ingen av disse", "Nan")
+reg_df["brev"] = reg_df["Brevtype"].copy()
+reg_df["brev"] = reg_df["brev"].str.replace("Ingen av disse", "Nan")
 
-model = OrderedModel.from_formula("dep_forstå ~ C(brev)", distr="logit", data=subset)
+model = OrderedModel.from_formula("dep_forstå ~ C(brev)", distr="logit", data=reg_df)
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 11")
 print(f"Formelen: {res.model.formula} \n \n")
@@ -220,7 +220,7 @@ print(res.summary())
 # %%
 # forståelse av begrunnelse og alder
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(brev) + C(Alder)", distr="logit", data=subset
+    "dep_forstå ~ C(brev) + C(Alder)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 12")
@@ -229,7 +229,7 @@ print(res.summary())
 # %%
 # forståelse av begrunnelse og alder og morsmål
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(brev) + C(Alder) + C(Morsmål)", distr="logit", data=subset
+    "dep_forstå ~ C(brev) + C(Alder) + C(Morsmål)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 13")
@@ -238,7 +238,7 @@ print(res.summary())
 # %%
 # forståelse av begrunnelse morsmål
 model = OrderedModel.from_formula(
-    "dep_forstå ~ C(brev) + C(Morsmål)", distr="logit", data=subset
+    "dep_forstå ~ C(brev) + C(Morsmål)", distr="logit", data=reg_df
 )
 res = model.fit(method="bfgs", disp=False)
 print("Modell nummer 14")
